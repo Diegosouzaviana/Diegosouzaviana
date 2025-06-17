e52d45d33e6a9ec05d49b37f95eafd61822e68eb
Diegosouzaviana <d-i-e-g-o1989@hotmail.com>
2025-06-17 10:18:51 -0300

    GMUD-17-06-2025

--- force-app/main/default/lwc/lXD_SelectPlan/lXD_SelectPlan.js
+++ force-app/main/default/lwc/lXD_SelectPlan/lXD_SelectPlan.js
@@ -32,12 +32,14 @@ export default class LXD_SelectPlan extends OmniscriptBaseMixin(LightningElement
     @api selectmultiple = false;
     @api crm;
     @track _products = [];
-
+    @track dadoparametro;
+    @track showButonTodos = true;
+    @track showButonMesmo = false;
+    @track exibeMuitosDados = 'fixed-bottom-button';
 
     get selectMultipleEnabled() {
         return this.selectmultiple.toString().toLocaleLowerCase() === 'false'
     }
-
     @api
     get productList() {
         return Array.isArray(this._products) ? this._products : [];
@@ -46,10 +48,123 @@ export default class LXD_SelectPlan extends OmniscriptBaseMixin(LightningElement
         this._products = this.productNormalizer(value);
     }
     showValidation;
-
     showSpinner = false;
     showError = false;
     cart = null;
+
+    connectedCallback() {
+        this.dadoparametro = this.omniJsonData?.dadoparametrojson;
+        console.log("dadoparametro", JSON.stringify(this.dadoparametro));
+        if (this._products.length == 0) {
+            this.exibeMuitosDados = 'fixed-bottom-button-sem-dado';
+        }
+        if (this._products.length > 0) {
+            this._products = this._products.map(plano => {
+                return {
+                    ...plano,
+                    lwctipodeplano: 'mesmoplano'
+                };
+            });
+        }
+    }
+    handleGetPlano(event) {
+        const buttonId = event.currentTarget.dataset.id;
+        if (buttonId === 'mesma-categoria') {
+            this.enableLoading();
+            this.showButonMesmo = false;
+            this.showButonTodos = true;
+            const tipodeplano = "mesmoplano";
+            const input = {
+                "seller_id": "7540",
+                "minDownloadSpeed": "1",
+                "migrated_provider": false,
+                "maxDownloadSpeed": "1000000",
+                "loyalty_contract": false,
+                "city": this.dadoparametro.city,
+                "category": this.dadoparametro.category,
+                "PlanoAntigoId": this.dadoparametro.PlanoAntigoId,
+                "currentPlan": this.dadoparametro.currentPlan,
+                "retentionPlan": this.dadoparametro.retentionPlan,
+                "sameCategory": true
+            };
+            this.handleCatalogOffers(input, tipodeplano);
+        } else if (buttonId === 'outro-planos') {
+            this.enableLoading();
+            this.showButonMesmo = true;
+            this.showButonTodos = false;
+            const tipodeplano = "outroplano";
+            const input = {
+                "seller_id": "7540",
+                "minDownloadSpeed": "1",
+                "migrated_provider": false,
+                "maxDownloadSpeed": "1000000",
+                "loyalty_contract": false,
+                "city": this.dadoparametro.city,
+                "category": this.dadoparametro.category,
+                "PlanoAntigoId": this.dadoparametro.PlanoAntigoId,
+                "currentPlan": this.dadoparametro.currentPlan,
+                "retentionPlan": this.dadoparametro.retentionPlan,
+                "sameCategory": false
+            };
+            this.handleCatalogOffers(input, tipodeplano);
+        }
+    }
+
+    handleCatalogOffers(input, tipodeplano) {
+        console.log('input', JSON.stringify(input));
+        const params = {
+            input: JSON.stringify(input),
+            sClassName: `${this._ns}IntegrationProcedureService`,
+            sMethodName: 'Every_IPCatalogOffers',
+            options: '{}',
+        };
+        this.omniRemoteCall(params, false).then(response => {
+            // console.log('response', JSON.stringify(response));
+            this.disableLoading();
+
+
+            if (response?.result?.IPResult?.productList?.length > 0) {
+                console.log('entrou ak 1');
+                this.exibeMuitosDados = 'fixed-bottom-button';
+                this._products = response.result.IPResult.productList.map(plano => {
+                    return {
+                        ...plano,
+                        lwctipodeplano: tipodeplano
+                    };
+                });
+                console.log("this._products", JSON.stringify(this._products));
+                // this._products = response.result.IPResult.productList;
+
+            } else {
+                if (response?.result?.IPResult?.plans == 0) {
+                    console.log('entrou ak 2');
+                    this.exibeMuitosDados = 'fixed-bottom-button-sem-dado';
+                    this._products = [];
+                    console.log("_products", JSON.stringify(this._products));
+                    const resultsToast = new ShowToastEvent({
+                        variant: "warning",
+                        message: "Nenhum plano semelhante disponível no momento. Clique em 'Outros planos' para ver mais opções.",
+                        title: "Planos indisponíveis"
+                    });
+                    this.dispatchEvent(resultsToast);
+                } else {
+                    console.log('entrou ak 3');
+                    this.exibeMuitosDados = 'fixed-bottom-button-sem-dado';
+                    this._products = [];
+                    const resultsToast = new ShowToastEvent({
+                        variant: "error",
+                        message: "Erro interno de servidor, abra um chamado GLPI",
+                        title: "Erro em busca planos"
+                    });
+                    this.dispatchEvent(resultsToast);
+                }
+            }
+
+
+        }).catch(error => {
+            console.error(error)
+        });
+    }
 
     @api
     checkValidity() {
@@ -98,6 +213,7 @@ export default class LXD_SelectPlan extends OmniscriptBaseMixin(LightningElement
             product.selected = true;
             product.orderItem = itemId;
         }
+
 
         const responseOS = {
             "selectedItem": this.productList[this.indexClick]
@@ -200,8 +316,6 @@ export default class LXD_SelectPlan extends OmniscriptBaseMixin(LightningElement
 
         if (desactivarSpinner) this.disableLoading();
     }
-
-
 
     /* addProduct */
     deleteProductsFromOrder() {
--- force-app/main/default/lwc/lwc_BotaoNextRemocaoPontoMesh/lwc_BotaoNextRemocaoPontoMesh.js
+++ force-app/main/default/lwc/lwc_BotaoNextRemocaoPontoMesh/lwc_BotaoNextRemocaoPontoMesh.js
@@ -8,50 +8,186 @@ export default class Lwc_BotaoNextRemocaoPontoMesh extends OmniscriptBaseMixin(L
     @track removerMesh;
     @track removerPontoMesh;
     @track quantidademeshSelecionado;
+    @track quantidadetv;
+    @track quantidademesh;
+    @track tvSelecionado;
+    @track removerTV;
+    @track removerPontoTV;
+    @track quantidadetvSelecionado;
     @api stepName;
     @api contador;
+    @api contadortv;
 
     connectedCallback() {
-        // console.log("stepName", JSON.stringify(this.stepName));
+        console.log("stepName", JSON.stringify(this.stepName));
     }
 
     validaSelecaomesh() {
         // console.log("stepName 1", JSON.stringify(this.stepName));
+        //mesh
         this.meshSelecionado = this.stepName.Formula2;
         this.quantidademesh = this.stepName.Formula3;
         this.quantidademeshSelecionado = this.stepName.Formula4;
         this.removerMesh = this.stepName.Block3.removerMesh;
         this.removerPontoMesh = this.stepName.Block3.removerPontoMesh;
         this.contador = this.contador;
+        // tv
+        this.tvSelecionado = this.stepName.Formula6;
+        this.quantidadetv = this.stepName.Formula7;
+        this.quantidadetvSelecionado = this.stepName.Formula8;
+        this.removerTV = this.stepName.Block9.removerTV;
+        this.removerPontoTV = this.stepName.Block9.removerPontoTV;
+        this.contadortv = this.contadortv;
 
 
-        // console.log("removerMesh", this.removerMesh);
-        // console.log("removerPontoMesh", this.removerPontoMesh);
-        // console.log("meshSelecionado", this.meshSelecionado);
-        // console.log("this.quantidademeshSelecionado", this.quantidademeshSelecionado);
-        // console.log("cantodorIqual", cantodorIqual);
+        console.log("removerMesh", this.removerMesh);
+        console.log("meshSelecionado", this.meshSelecionado);
+        console.log("removerPontoMesh", this.removerPontoMesh);
+        console.log("quantidademesh", this.quantidademesh);
+        console.log("this.quantidademeshSelecionado", this.quantidademeshSelecionado);
+        console.log("contador", this.contador);
 
-        if (this.removerMesh == "Não") {
+        console.log("removerTV", this.removerTV);
+        console.log("tvSelecionado", this.tvSelecionado);
+        console.log("removerPontoTV", this.removerPontoTV);
+        console.log("quantidadetv", this.quantidadetv);
+        console.log("quantidadetvSelecionado", this.quantidadetvSelecionado);
+        console.log("contadorTV", this.contadortv);
+
+        // if (this.removerMesh == "Não") {
+        //     this.nextButton();
+        // } else {
+        //     if (this.removerMesh == "Sim" && this.removerPontoMesh == null && this.meshSelecionado == false) {
+        //         // console.log("entrou 1");
+        //         this.nextButton();
+
+        //     } else if (this.removerMesh == undefined && this.removerPontoMesh == null && this.meshSelecionado == false) {
+        //         // console.log("entrou 3");
+        //         this.nextButton();
+
+        //     } else if (this.removerMesh == "Sim" && this.removerPontoMesh > 0 && this.removerPontoMesh <= this.contador && this.meshSelecionado == false && this.quantidademesh == 0) {
+        //         // console.log("entrou 4");
+        //         this.mostrarToast('Selecione pontos Mesh a serem removidos.', 'warning');
+        //     } else if (this.removerMesh == "Sim" && this.removerPontoMesh != null && this.meshSelecionado == false && this.quantidademeshSelecionado == true) {
+        //         // console.log("entrou 5");
+        //         // this.mostrarToast('Por favor, selecione pontos Mesh a serem removidos.', 'warning');
+        //     } else {
+        //         // console.log("entrou 6");
+        //         this.nextButton();
+        //     }
+        // }
+
+        // Chamada da lógica unificada
+        // if ((this.removerMesh == "Nao" || this.removerMesh == undefined) && (this.removerTV == "Nao" || this.removerTV == undefined)) {
+        //     this.mostrarToast('Você deve selecionar os panto de remoção (TV e Mesh).', 'warning');
+        // } else {
+        //     if (this.removerMesh == "Sim") {
+        //         this.validaTipo(
+        //             this.removerMesh,
+        //             this.removerPontoMesh,
+        //             this.meshSelecionado,
+        //             this.quantidademesh,
+        //             this.quantidademeshSelecionado,
+        //             this.contador,
+        //             'Mesh'
+        //         );
+        //     }
+
+        //     if (this.removerTV == "Sim") {
+        //         this.validaTipo(
+        //             this.removerTV,
+        //             this.removerPontoTV,
+        //             this.tvSelecionado,
+        //             this.quantidadetv,
+        //             this.quantidadetvSelecionado,
+        //             this.contadortv,
+        //             'TV'
+        //         );
+        //     }
+        // }
+        const nenhumSelecionado = (this.removerMesh !== "Sim" && this.removerTV !== "Sim");
+
+        if (nenhumSelecionado) {
+            this.mostrarToast('Você deve selecionar os pontos de remoção (TV e Mesh).', 'warning');
+            return;
+        }
+
+        if (this.removerMesh === "Sim") {
+            this.validarRemocao({
+                remover: this.removerMesh,
+                ponto: this.removerPontoMesh,
+                selecionado: this.meshSelecionado,
+                quantidade: this.quantidademesh,
+                quantidadeSelecionado: this.quantidademeshSelecionado,
+                contador: this.contador,
+                tipo: 'Mesh'
+            });
+        }
+
+        if (this.removerTV === "Sim") {
+            this.validarRemocao({
+                remover: this.removerTV,
+                ponto: this.removerPontoTV,
+                selecionado: this.tvSelecionado,
+                quantidade: this.quantidadetv,
+                quantidadeSelecionado: this.quantidadetvSelecionado,
+                contador: this.contadortv,
+                tipo: 'TV'
+            });
+        }
+    }
+    validarRemocao({ remover, ponto, selecionado, quantidade, quantidadeSelecionado, contador, tipo }) {
+        const nenhumSelecionado = ponto == null && selecionado === false;
+
+        if (remover !== "Sim") {
             this.nextButton();
+            return;
+        }
+
+        if (nenhumSelecionado && (remover === "Sim" || remover === undefined)) {
+            this.nextButton();
+            return;
+        }
+
+        const dentroDoLimite = ponto > 0 && ponto <= contador;
+        const nenhumDisponivel = quantidade === 0;
+
+        if (dentroDoLimite && !selecionado && nenhumDisponivel) {
+            this.mostrarToast(`Selecione pontos ${tipo} a serem removidos.`, 'warning');
+            return;
+        }
+
+        if (ponto != null && !selecionado && quantidadeSelecionado) {
+            // this.mostrarToast(`Por favor, selecione pontos ${tipo} a serem removidos.`, 'warning');
+            return;
+        }
+
+        this.nextButton();
+    }
+    validaTipo(remover, ponto, selecionado, quantidade, quantidadeSelecionado, contador, tipo) {
+        if (remover == "Não") {
+            console.log("1");
+            this.nextButton();
+            return;
+        } else if (remover == "Sim" && ponto == null && selecionado == false) {
+            console.log("2");
+            this.nextButton();
+            return;
+        } else if (remover == undefined && ponto == null && selecionado == false) {
+            console.log("3");
+            this.nextButton();
+            return;
+        } else if (remover == "Sim" && ponto > 0 && ponto <= contador && selecionado == false && quantidade == 0) {
+            console.log("4");
+            this.mostrarToast(`Selecione pontos ${tipo} a serem removidos.`, 'warning');
+            return;
+        } else if (remover == "Sim" && ponto != null && selecionado == false && quantidadeSelecionado == true) {
+            console.log("5");
+            return;
+            // this.mostrarToast(`Por favor, selecione pontos ${tipo} a serem removidos.`, 'warning');
         } else {
-            if (this.removerMesh == "Sim" && this.removerPontoMesh == null && this.meshSelecionado == false) {
-                // console.log("entrou 1");
-                this.nextButton();
-
-            } else if (this.removerMesh == undefined && this.removerPontoMesh == null && this.meshSelecionado == false) {
-                // console.log("entrou 3");
-                this.nextButton();
-
-            } else if (this.removerMesh == "Sim" && this.removerPontoMesh > 0 && this.removerPontoMesh <= this.contador && this.meshSelecionado == false && this.quantidademesh == 0) {
-                // console.log("entrou 4");
-                this.mostrarToast('Selecione pontos Mesh a serem removidos.', 'warning');
-            } else if (this.removerMesh == "Sim" && this.removerPontoMesh != null && this.meshSelecionado == false && this.quantidademeshSelecionado == true) {
-                // console.log("entrou 5");
-                // this.mostrarToast('Por favor, selecione pontos Mesh a serem removidos.', 'warning');
-            } else {
-                // console.log("entrou 6");
-                this.nextButton();
-            }
+            console.log("6");
+            this.nextButton();
         }
     }
 
